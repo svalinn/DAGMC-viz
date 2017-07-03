0aaabb8425f9f3d37b368324c2417598afacfde5
Lane Schultz <lsschultz@wisc.edu>
2017-07-03 10:52:20 -0500

    Removed Shading.

--- WindowSettings.py
+++ WindowSettings.py
@@ -6,18 +6,27 @@ from visit import *
 def WindowSettings():
     """Modify window settings."""
 
-    # Set window attributes for saving
+    # Set window attributes for saving.
     Attribute = SaveWindowAttributes()
 
     Attribute.format = Attribute.PNG
 
     Attribute.fileName = "./Images/sample"
 
-    Attribute.width = 500
-    Attribute.height = 2000
+    Attribute.width = 200
+    Attribute.height = 100
 
     SetSaveWindowAttributes(Attribute)
 
+    # Set lighting attributes.
     light = GetLight(0)
     light.type = light.Ambient
     SetLight(0, light)
+
+    # Set legend options.
+    Attribute = AnnotationAttributes()
+
+    Attribute.databaseInfoFlag = 0
+    Attribute.legendInfoFlag = 1
+
+    SetAnnotationAttributes(Attribute)
