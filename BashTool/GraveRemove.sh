02a2d257e93b41fa174165f8ad0ddfe70afabef1
Lane Schultz <lsschultz@wisc.edu>
2017-07-11 15:49:44 -0500

    Attempt to automate mbconvert to remove graveyard.

--- BashTool/GraveRemove.sh
+++ BashTool/GraveRemove.sh
@@ -2,13 +2,21 @@
 DATAPATH=../PythonTool/Data
 cd "${DATAPATH}"
 
-echo -e "File for removal of graveyard: "
+printf "Include file extensions for naming. \n"
+
+echo -e "File (h5m) for removal of graveyard: "
 read WithGrave
 
 mbgsets $WithGrave > ../../BashTool/DataFileInfo/Structure.txt
 
 STRUCTUREPATH=../../BashTool/DataFileInfo
 cd "${STRUCTUREPATH}"
-pwd
 
-grep s3376 Structure.txt
+sed '1,/^$/d' Structure.txt > Relations.txt
+sed '/^$/,$ d' Structure.txt > Geometry.txt
+
+GraveID=$(grep graveyard Structure.txt | awk '{print $1;}')
+
+RelationToGraveID=$(grep $GraveID Relations.txt | awk '{print $3;}' | sed 's/;$//')
+
+grep $RelationToGraveID Relations.txt > GraveGeometry.txt
