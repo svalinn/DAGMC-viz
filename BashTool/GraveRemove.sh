147feb1d6784e9e1addeaef0241027d69211e732
Lane Schultz <lsschultz@wisc.edu>
2017-07-11 16:13:15 -0500

    Added commenting for clarification.

--- BashTool/GraveRemove.sh
+++ BashTool/GraveRemove.sh
@@ -7,16 +7,24 @@ printf "Include file extensions for naming. \n"
 echo -e "File (h5m) for removal of graveyard: "
 read WithGrave
 
+# Save the relations.
 mbgsets $WithGrave > ../../BashTool/DataFileInfo/Structure.txt
 
 STRUCTUREPATH=../../BashTool/DataFileInfo
 cd "${STRUCTUREPATH}"
 
+# Delete everything before line break.
 sed '1,/^$/d' Structure.txt > Relations.txt
+# Delete everything after line break.
 sed '/^$/,$ d' Structure.txt > Geometry.txt
 
+# Get graveyard ID.
 GraveID=$(grep graveyard Structure.txt | awk '{print $1;}')
 
+# Get first level ID related to graveyard.
 RelationToGraveID=$(grep $GraveID Relations.txt | awk '{print $3;}' | sed 's/;$//')
 
-grep $RelationToGraveID Relations.txt > GraveGeometry.txt
+# Get second level ID related to graveyard.
+SecondIDs=$(grep $RelationToGraveID Relations.txt > GraveGeometry.txt)
+
+grep $SecondIDs Geometry.txt > Geology.txt
