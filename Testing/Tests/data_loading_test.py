a7d2274daf88043f3569fe8269defc10f421eb22
Piper Lincoln <plincoln@wisc.edu>
2020-02-10 10:26:44 -0600

    testing image generation

--- Testing/Tests/data_loading_test.py
+++ Testing/Tests/data_loading_test.py
@@ -45,9 +45,10 @@ def test_visit_config():
     diff = main.diff_files("VisitDefaultOutput.session", session_file)
     assert len(diff) == 4
 
-
+"""
 def test_cleanup():
     """
-    Remove the files written to disk by this class of tests.
+    #Remove the files written to disk by this class of tests.
     """
     os.system('rm visit* *.stl *.vtk *.session')
+"""
