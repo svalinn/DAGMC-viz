54916db83c4d7f8fc358375d96b18a12e93202fa
Piper Lincoln <plincoln@wisc.edu>
2020-02-28 10:36:36 -0600

    ensure the readme is opened correctly

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+VERSION = '0.1'
+
 from setuptools import setup
+from os import path
 from io import open
 
-
-VERSION = '0.1'
+here = path.abspath(path.dirname(__file__))
 
 
 def main():
@@ -15,7 +17,7 @@ def main():
         "name": "dagmc-viz",
         "version": VERSION,
         "description": 'DAGMC Visualization Tools',
-        "long_description": open('README.md').read(),
+        "long_description": open(path.join(here,'README.md'), encoding = 'utf-8').read(),
         "author": 'Svalinn Development Team',
         "url": 'https://github.com/svalinn/DAGMC-viz',
         "packages": packages,
